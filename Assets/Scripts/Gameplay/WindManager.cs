4c2acf83f799af70ed725fbc57824fc596a073e2
Tixewi <tixewi@yahoo.de>
2021-01-30 23:35:07 +0100

    Static Clouds; Cloud Prefabs

--- Assets/Scripts/Gameplay/WindManager.cs
+++ Assets/Scripts/Gameplay/WindManager.cs
@@ -11,12 +11,36 @@ namespace GGJ.Gameplay
         [Range(0f, 1f)] [SerializeField] float maxWindStrength;
         [SerializeField] GameObject[] cloudPrefabs;
         [SerializeField] Vector3 cloudSpawnVector;
-        [SerializeField] float cloudDistanceUntilDeath;
-        [Range(0f, 1f)] [SerializeField] float cloudAmount;
+        [SerializeField] Vector3 cloudLayerSize;
+        [Range(1, 20)] [SerializeField] int cloudAmount;
 
         public Vector2 GetWindSpeed()
         {
             return windDirection * Random.Range(minWindStrength, maxWindStrength);
+        }
+
+        void Start()
+        {
+            SpawnStaticClouds();
+        }
+
+        void SpawnStaticClouds()
+        {
+            for (int i = 0; i < cloudAmount; i++)
+            {
+                float x = Random.Range(cloudSpawnVector.x, cloudLayerSize.x);
+                float y = Random.Range(cloudSpawnVector.y, cloudLayerSize.y);
+                float z = Random.Range(cloudSpawnVector.z, cloudLayerSize.z);
+                Vector3 spawn = new Vector3(x, y, z);
+
+                int cloudIndex = Random.Range(0, cloudPrefabs.Length);
+
+                Instantiate(cloudPrefabs[cloudIndex], spawn, Quaternion.identity);
+
+                //if (windDirection == Vector2.zero)
+                //{
+                //}
+            }
         }
     }
 }
