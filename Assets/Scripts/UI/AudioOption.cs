25e8ce50c23af23028ef79baf1efe0e2b86378ac
Benny <benny.zoeller@gmail.com>
2021-01-31 13:05:38 +0100

    Implemented Music
    - Added Music to Project
    - Music-Singleton
    - Option-Slider for Music

---
+++ Assets/Scripts/UI/AudioOption.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioOption : MonoBehaviour
+{
+    [SerializeField]
+    Slider slider;
+
+    private void Start()
+    {
+        slider.value = PlayMusic.Instance.GetStrength();
+    }
+
+    public void SetStrength(float value)
+    {
+        PlayMusic.Instance.SetMusicStrength(value);
+    }
+}
